63f9e05d4ebc80a511b2a52ba51cd17b85517757
Anderson <asilva130498@gmail.com>
2021-08-05 01:03:31 -0300

    config heroku

--- gestor/settings.py
+++ gestor/settings.py
@@ -1,8 +1,9 @@
 import os
 from pathlib import Path
-from decouple import config
+from prettyconf import Configuration
 from dj_database_url import parse as dburl
 
+config = Configuration()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
